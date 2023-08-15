0aea1eb8d8e32bf84151cd27b8b7fd51ba6c4c9f
Beratttt <beratozen2510@gmail.com>
2023-08-15 14:50:40 +0300

    Add moongoose

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-import express from "express";
-import ejs from "ejs";
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const Post = require("./models/Post");
 const app = express();
+
+// DB Connection
+mongoose
+  .connect("mongodb://localhost/cleanblog-test-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Databaseye bağlandı"));
 
 // Template Engine
 app.set("view engine", "ejs");
 
 // middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+
+  res.render("index", { posts });
 });
 
 app.get("/about", (req, res) => {
@@ -19,6 +33,22 @@ app.get("/about", (req, res) => {
 
 app.get("/add_post", (req, res) => {
   res.render("add_post");
+});
+
+app.post("/posts", async (req, res) => {
+  const { title, detail } = req.body;
+
+  const post = new Post({
+    title,
+    detail,
+  });
+
+  await post
+    .save()
+    .then(() => console.log("Veri Eklendi"))
+    .catch((err) => console.log("Veri eklenemedi. Hata = " + err));
+
+  res.redirect("/");
 });
 
 const port = 3000;
