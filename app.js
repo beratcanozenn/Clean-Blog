f68b38e84a295d386c4c8e371329f74d060adf2a
Beratttt <beratozen2510@gmail.com>
2023-08-15 09:26:19 +0300

    Setting HTML pages to ejs

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 import express from "express";
-
+import ejs from "ejs";
 const app = express();
+
+// Template Engine
+app.set("view engine", "ejs");
+
+// middleware
+app.use(express.static("public"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
 
 const port = 3000;
 app.listen(port, () => {
